53fbb242c5c95e115c4f918ff0ad7304079a8b43
Ashish Subedi <ashishsubedi10@outlook.com>
2018-12-08 01:43:41 +0545

    Created MySQL Test Database and Tested user Registration

--- app.js
+++ app.js
@@ -1,35 +1,84 @@
 const port = 3000;
+//Importing/Including required module 
 
 const express = require('express');
 const path = require('path');
 var bodyParser = require('body-parser');
 
+//TEST
+const mysql = require('mysql');
+
+//Initializing express function
 const app = express();
 
+//Serving static files
 app.use('/css-mat',express.static(__dirname+'/node_modules/materialize-css/dist/css'));
 app.use('/js-mat',express.static(__dirname+'/node_modules/materialize-css/dist/js'));
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use(express.static(path.join(__dirname,'public')));
 
-
+//Using bodyParser Parser; required for parsing request data
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
+
+//GET: localhost:port/
 app.get('/', (req,res) =>{
     res.sendFile(path.join(__dirname,"/views/index.html"));
 });
 
+//GET: localhost:port/register
+
 app.get('/register', (req,res) =>{
     res.sendFile(path.join(__dirname,"/views/register.html"));
 });
+
+//GET: localhost:port/login
+
 app.get('/login', (req,res) =>{
     res.sendFile(path.join(__dirname,"/views/register.html"));
 });
 
+//POST: localhost:post/registered
+//Handles registration
 app.post('/registered',(req,res)=>{
-    res.end("User Registered! Welcome, "+ req.body.fullName);
+    var currentdate = new Date(); 
+    var datetime = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+    
+
+    con.query("INSERT INTO `users` (`First Name`, `Last Name`, `Email`, `Password`, `Date Created`, `Logged In`) VALUES (?, ?, ?, ?, ?, '0')",
+        [req.body.firstName,req.body.lastName,req.body.email,req.body.password,datetime],(err,rows,fields)=>{
+
+            if(err) {console.log(err); return;}
+            //TODO: Redirect to Login Page
+            res.send("Welcome to the site, "+ req.body.firstName);
+    });
 });
 
 
+
+
+
+//listen on port and running the server
 app.listen(port,()=>{
     console.log(`Server running at port ${port}`);
+});
+
+//Connecting to MySQL Database
+//TEST CODE.
+const con = mysql.createConnection({
+    host:"localhost",
+    user:'root',
+    password:"",
+    database:"test"
+});
+
+con.connect((err)=>{
+    if(err) console.log(err);
+    console.log("Connected!");
 });
--- test_database.js
+++ test_database.js
@@ -1,12 +1,45 @@
+const express = require('express');
+const app = express();
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+const port = 3000;
 
 const con = mysql.createConnection({
     host:"localhost",
     user:'root',
-    password:"root"
+    password:"",
+    database:"test"
 });
 
-con.connect(()=>{
-    if(err) console.error("Connection Error!");
+con.connect((err)=>{
+    if(err) console.log(err);
     console.log("Connected!");
+});
+
+app.get('/',(req,res)=>{
+    res.end("Hello World");
+});
+
+app.get('/users',(req,res)=>{
+    
+    con.query('SELECT * FROM users',(err,rows,fields)=>{
+        if(err) console.log(err);
+        res.send(rows);
+    });
+});
+app.get('/users/:_id',(req,res)=>{
+
+    con.query("SELECT * FROM users where id = ?",[req.params._id],(err,rows)=>{
+        if(err) console.log(err);
+        res.send(rows);
+    });
+
+});
+
+app.listen(port,()=>{
+    console.log("Server running on port "+ port);
 });
