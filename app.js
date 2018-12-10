6cbc91a3b1e85e2c3b16758bba670d32c814a8c2
Ashish Subedi <ashishsubedi10@outlook.com>
2018-12-11 00:07:20 +0545

    Added Jquery loading for Registration

--- app.js
+++ app.js
@@ -42,20 +42,23 @@ app.get('/login', (req,res) =>{
 //POST: localhost:post/registered
 //Handles registration
 app.post('/registered',(req,res)=>{
-    var currentdate = new Date(); 
-    var datetime = currentdate.getDate() + "/"
-                + (currentdate.getMonth()+1)  + "/" 
-                + currentdate.getFullYear() + " @ "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();
-              
+    //var currentdate = new Date(); 
+    // var datetime = currentdate.getDate() + "/"
+    //             + (currentdate.getMonth()+1)  + "/" 
+    //             + currentdate.getFullYear() + " @ "  
+    //             + currentdate.getHours() + ":"  
+    //             + currentdate.getMinutes() + ":" 
+    //             + currentdate.getSeconds();
+    var datetime = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+    var dob = formatDate(req.body.birth);
+    var c = {dc: datetime,
+    dob : dob};
     con.query("INSERT INTO `users` (`First Name`, `Last Name`, `Email`, `Password`, `Date Created`,`Date of Birth`, `Gender`, `Logged In`) VALUES (?, ?, ?, ?, ?,?,?, '0')",
-        [req.body.firstName,req.body.lastName,req.body.email,req.body.password,datetime,req.body.birth,req.body.gender],(err,rows,fields)=>{
+        [req.body.firstName,req.body.lastName,req.body.email,req.body.password,datetime,dob,req.body.gender],(err,rows,fields)=>{
 
             if(err) {console.log(err); return;}
             //TODO: Redirect to Login Page
-            res.send("Welcome to site,"+ req.body.firstName);
+            res.redirect("/");
         
     });
 });
@@ -78,3 +81,15 @@ con.connect((err)=>{
     if(err) console.log(err);
     console.log("Connected!");
 });
+
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+}
