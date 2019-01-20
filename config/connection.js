f9f7525d6723cd108240a3e4751550509140799c
Ashish Subedi <ashishsubedi10@outlook.com>
2019-01-21 00:04:22 +0545

    Updated connection settings

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,10 @@ const keys = require('./keys');
 
 
 const con = mysql.createConnection({
-    host: process.env.HOST || keys.mysql.host,
-    user: process.env.USERNAME || keys.mysql.username,
-    password: process.env.PASSWORD || keys.mysql.password,
-    database: process.env.DATABASE || keys.mysql.database
+    host: process.env.MySqlHost || keys.mysql.host,
+    user: process.env.MySqlUsername || keys.mysql.username,
+    password: process.env.MySqlPassword || keys.mysql.password,
+    database: process.env.MySqlDatabaseName || keys.mysql.database
 });
 
 module.exports = con;
